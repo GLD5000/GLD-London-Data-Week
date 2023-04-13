e9aac93aacd68501c3964edf6dee94bc116b575f
GLD5000 <garethlouisdevlin@gmail.com>
2023-04-13 12:51:22 +0100

    Added tab trap to modal

--- src/sections/body/EventModal.tsx
+++ src/sections/body/EventModal.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useRef } from 'react';
+import CloseSvg from '../../icons/CloseSvg';
+
 function lookupMonthName(number: number) {
   const lookupArray = [
     'January',
@@ -20,6 +23,16 @@ export default function EventModal({ close, event }: { close: () => void; event:
   const { Day, Date, Name, Location, DeliveryPartner, TypeofEvent } = event;
   const [monthNumber, dayOfMonth, year] = Date.split('/');
   const monthName = lookupMonthName(Number(monthNumber));
+  const initialFocus = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    let run = true;
+    if (run && initialFocus.current != null) initialFocus.current.focus();
+    return () => {
+      run = false;
+    };
+  }, [initialFocus]);
+
   return (
     <div
       //   onClick={close}
@@ -28,14 +41,24 @@ export default function EventModal({ close, event }: { close: () => void; event:
       className="fixed top-0 left-0 m-auto bg-semiBlue h-screen w-screen z-[999] flex"
     >
       <div
-        className="autoFocus={true} relative sm:w-fit w-screen h-screen sm:h-fit bg-white text-darkblue m-auto p-12"
+        className="relative sm:w-fit w-screen h-screen sm:h-fit bg-white text-darkblue m-auto p-12"
         // onClick={(e) => {
         //   e.stopPropagation();
         //   console.log('modal');
         // }}
       >
-        <button id="close-btn" type="button" onClick={close} className="w-12 h-12 bg-palegrey absolute top-0 right-0">
-          close
+        <button
+          ref={initialFocus}
+          id="close-btn"
+          aria-label="close-information-box"
+          type="button"
+          onClick={close}
+          onKeyDown={(e) => {
+            if (e.key !== 'Tab') close();
+          }}
+          className="w-10 h-10 bg-palegrey hover:transition text-darkblue focus:text-palegrey focus:transition focus:bg-darkblue hover:bg-darkblue hover:text-white rounded-full p-1 absolute top-2 right-2"
+        >
+          <CloseSvg />
         </button>
         <div>
           <div className="my-4 bg-lightgreen py-2 px-4">
@@ -61,7 +84,18 @@ export default function EventModal({ close, event }: { close: () => void; event:
             <span className="font-bold">Time: </span>3pm
           </p>
           <p className="w-fit mx-auto mt-8 mb-2 text-xl">
-            <span className="font-bold">Registration: </span>www.google.com
+            <span className="font-bold">Registration: </span>
+            <a
+              onBlur={() => {
+                console.log('blur');
+                if (initialFocus.current != null) initialFocus.current.focus();
+              }}
+              href="https://www.google.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              www.google.com
+            </a>
           </p>
           <p className="w-fit mx-auto my-2 text-xl">
             <span className="font-bold">More Info: </span>
