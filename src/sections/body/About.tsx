3288b2e13ddd998d3183d33a65a45f2fe7df1639
GLD5000 <garethlouisdevlin@gmail.com>
2023-04-17 21:55:18 +0100

    Added about content

--- src/sections/body/About.tsx
+++ src/sections/body/About.tsx
@@ -24,28 +24,33 @@ export default function About() {
       ref={elementRef}
       className="relative min-h-[80vh] scroll-m-32 sm:px-20 bg-transparent snap-start w-body-sm min-w-body max-w-body sm:w-body mx-auto"
     >
-      {/* <div className=" -left-[3%] top-[7%] md:top-[27%] h-4 w-4 absolute bg-yellow hover:scale-150 hover:transition " />
-      <div className=" left-[85%] top-[3%] md:top-[25%] h-5 w-5 absolute dark:bg-lightgreen bg-darkgreen hover:scale-150 hover:transition" />
-      <div className=" left-[5%] top-[86%] md:top-[65%] h-6 w-6 absolute bg-pink hover:scale-150 hover:transition" />
-      <div className=" left-[15%] top-[92%] md:top-[64%] h-5 w-5 absolute dark:bg-lightgreen bg-darkgreen hover:scale-150 hover:transition" />
-      <div className=" left-[75%] top-[90%] md:top-[62%] h-4 w-4 absolute bg-pink hover:scale-150 hover:transition" /> */}
+      <div className="text-xl w-80 mx-auto">
+        <div className="flex">
+          <p className="w-fit my-8">
+            Data is part of our everyday lives as Londoners, from choosing the
+            <span className=" dark:text-lightgreen text-darkgreentxt font-bold"> best Tube route</span> to finding your
+            <span className=" dark:text-lightgreen text-darkgreentxt font-bold"> next pair of trainers</span>.
+          </p>
+        </div>
 
-      <div className=" dark:bg-lightgreen bg-darkgreen text-darkblue w-fit py-12 px-4 sm:px-20 mt-20 mx-auto sm:mr-auto font-bold z-0">
-        <p className=" text-6xl mb-4 mx-auto w-fit ">July 3-9</p>
-        <p className=" text-2xl mr-auto w-fit text-center">Join us for a free, city-wide</p>
-        <p className=" text-2xl mr-auto w-fit text-center">festival of data for everyone</p>
+        <div className="flex">
+          <p className="w-fit my-8">
+            Join us at London Data Week for a citywide festival about data to
+            <span className=" dark:text-yellow text-darkpink font-bold"> learn </span>,{' '}
+            <span className=" dark:text-lightgreen text-darkgreentxt font-bold"> create</span>,
+            <span className=" dark:text-pink text-darkpink font-bold"> discuss</span>, and
+            <span className=" dark:text-lightgreen text-darkgreentxt font-bold"> explore</span> how to use data to shape
+            our city for the better.
+          </p>
+        </div>
+
+        <div className="flex">
+          <p className="w-fit my-8">
+            Explore our schedule to discover workshops, comedy events, cycling challenges and more! More events to be
+            announced in May 2023.
+          </p>
+        </div>
       </div>
-      <img className="rounded-2xl w-full" role="presentation" src="./data cropped.png" alt="Data in the Public, For" />
-      {/* <div className="text-6xl">
-        <p className="w-fit mx-auto mt-10 text-center">
-          <span className=" dark:text-lightgreen text-center text-darkgreentxt font-bold">Data</span> in the{' '}
-          <span className=" dark:text-lightgreen text-center text-darkgreentxt font-bold">public</span>,
-        </p>
-        <p className="w-fit mx-auto text-center">
-          for the <span className=" dark:text-lightgreen text-darkgreentxt font-bold">public</span>.
-        </p>
-      </div>
-      <p className="w-fit mx-auto text-2xl mt-10">#LDW2023</p> */}
     </section>
   );
 }
