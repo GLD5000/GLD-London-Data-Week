65d21b8ff480fa9a14bf86cc0ddaa4af7111f6f2
GLD5000 <garethlouisdevlin@gmail.com>
2023-04-18 23:11:51 +0100

    Updated hero and header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -19,15 +19,15 @@ export default function Header({
   return (
     <header
       id="top"
-      className="sticky top-0 left-0 right-0 z-[998] grid py-3 w-screen h-fit grid-cols-frAutoFr content-center border-none border-border dark:border-border-dk bg-bg dark:bg-bg-dk"
+      className="sticky top-0 left-0 right-0 z-[996] grid py-3 w-screen h-fit content-center  bg-bg dark:bg-bg-dk"
     >
-      <div className=" col-start-2 ">
+      <div className=" ">
         <div
-          className=" flex justify-between w-body-sm min-w-body max-w-body flex-wrap
+          className=" flex justify-between mx-auto w-body-sm min-w-body max-w-body flex-wrap
 sm:w-body "
         >
           <a
-            className="flex h-12 mr-auto my-auto ml-0 w-[7rem] flex-wrap items-center "
+            className="flex h-12 mr-auto my-auto ml-0 max-w-[7rem] flex-wrap items-center "
             href={originURL}
             aria-label="Go To London Data Week Website"
           >
