2506ad1eb4e8d7e64b3510d4bdbbf16aff7677f2
GLD5000 <garethlouisdevlin@gmail.com>
2023-04-18 08:47:31 +0100

    Updated hero alignment

--- src/sections/body/Hero.tsx
+++ src/sections/body/Hero.tsx
@@ -25,7 +25,7 @@ export default function Hero() {
     <section
       id="hero-section"
       ref={elementRef}
-      className=" relative min-h-screen items-center sm:px-16 lg:snap-start w-body-sm min-w-body max-w-body sm:w-body mx-auto "
+      className=" relative min-h-screen items-center lg:snap-start w-body-sm min-w-body max-w-body sm:w-body mx-auto "
     >
       <div className="flex h-full flex-col justify-between">
         <div className="h-fit grid gap-20 mt-32 ">
@@ -36,15 +36,15 @@ export default function Hero() {
           {/* <div className=" ml-auto md:w-3/5 fade-in">
             <GroupLogoWhite />
           </div> */}
-          <div className=" m-auto text-center w-full fade-in">
-            <p className=" text-3xl sm:text-6xl mb-6 mx-auto w-fit ">July 3-9</p>
-            <div className="text-3xl mx-auto text-center w-fit justify-center sm:text-6xl flex flex-row flex-wrap gap-2">
-              <p className="w-fit text-center">
-                <span className=" dark:text-lightgreen text-center text-darkgreentxt font-bold">Data</span> in the{' '}
-                <span className=" dark:text-lightgreen text-center text-darkgreentxt font-bold">public</span>,
+          <div className=" m-auto grid gap-8 sm:gap-16 text-center w-full fade-in">
+            <p className=" text-3xl sm:text-[3.5rem] mx-auto w-fit font-bold ">July 3-9</p>
+            <div className="text-3xl mx-auto text-center w-fit justify-center sm:text-[3.5rem] flex flex-row flex-wrap gap-2">
+              <p className="w-fit text-center my-2">
+                <span className="dark:text-lightgreen text-center text-darkgreentxt font-bold my-2">Data</span> in the{' '}
+                <span className="dark:text-lightgreen text-center text-darkgreentxt font-bold my-2">public</span>,
               </p>
-              <p className="w-fit text-center">
-                for the <span className=" dark:text-lightgreen text-darkgreentxt font-bold">public</span>.
+              <p className="w-fit text-center my-2">
+                for the <span className=" dark:text-lightgreen text-darkgreentxt font-bold my-2">public</span>.
               </p>
             </div>{' '}
           </div>
