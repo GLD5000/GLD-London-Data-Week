f2a0af2727e54bf2adab3b2217ca5de042996aea
GLD5000 <garethlouisdevlin@gmail.com>
2023-04-18 09:01:00 +0100

    Updated Hero Layout

--- src/sections/body/Hero.tsx
+++ src/sections/body/Hero.tsx
@@ -25,10 +25,10 @@ export default function Hero() {
     <section
       id="hero-section"
       ref={elementRef}
-      className=" relative min-h-screen items-center lg:snap-start w-body-sm min-w-body max-w-body sm:w-body mx-auto "
+      className=" relative min-h-[85vh] lg:min-h-screen items-center lg:snap-start pt-20 w-body-sm min-w-body max-w-body sm:w-body mx-auto "
     >
-      <div className="flex h-full flex-col justify-between">
-        <div className="h-fit grid gap-20 mt-32 ">
+      <div className="flex h-full flex-col justify-between gap-8">
+        <div className="h-fit grid gap-20 ">
           <div className="pr-[6.5%] bg-darkblue rounded-3xl overflow-clip py-6">
             <AnimatedLogo />
           </div>
@@ -36,9 +36,9 @@ export default function Hero() {
           {/* <div className=" ml-auto md:w-3/5 fade-in">
             <GroupLogoWhite />
           </div> */}
-          <div className=" m-auto grid gap-8 sm:gap-16 text-center w-full fade-in">
-            <p className=" text-3xl sm:text-[3.5rem] mx-auto w-fit font-bold ">July 3-9</p>
-            <div className="text-3xl mx-auto text-center w-fit justify-center sm:text-[3.5rem] flex flex-row flex-wrap gap-2">
+          <div className=" m-auto grid gap-8 md:gap-16 text-center w-full fade-in">
+            <p className=" text-3xl md:text-[3.5rem] mx-auto w-fit font-bold ">July 3-9</p>
+            <div className="text-3xl mx-auto text-center w-fit justify-center md:text-[3.5rem] flex flex-row flex-wrap gap-2">
               <p className="w-fit text-center my-2">
                 <span className="dark:text-lightgreen text-center text-darkgreentxt font-bold my-2">Data</span> in the{' '}
                 <span className="dark:text-lightgreen text-center text-darkgreentxt font-bold my-2">public</span>,
@@ -50,7 +50,7 @@ export default function Hero() {
           </div>
         </div>
         <a
-          className="h-10 w-10 mx-auto mt-auto mb-24 sm:mb-16 text-darkblue dark:text-white hover:transition hover:text-darkgreen dark:hover:text-pink focus-within:transition focus-within:text-darkgreen dark:focus-within:text-pink"
+          className="h-10 w-10 mx-auto mt-auto mb-24 md:mb-16 text-darkblue dark:text-white hover:transition hover:text-darkgreen dark:hover:text-pink focus-within:transition focus-within:text-darkgreen dark:focus-within:text-pink"
           href="#about-section"
           aria-label="scroll down"
           id="scroll-down"
