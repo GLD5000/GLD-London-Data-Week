38fda0baff5c4bba8cd2d23fafc79a79b9b2c444
GLD5000 <garethlouisdevlin@gmail.com>
2023-04-20 16:17:17 +0100

    Added ATI and LOTI logos and urls

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,26 +1,45 @@
-import GroupLogoWhite from '../assets/GroupLogoWhite';
+import AtiSvg from '../assets/AtiSvg';
+import LotiSvg from '../assets/LotiSvg';
 import GldSvg from '../icons/GldSvg';
+import HorizontalDivider from '../icons/HorizontalDivider';
 import ExternalLink from './header/ExternalLink';
 
 export default function Footer() {
   return (
     <footer id="footer" className=" flex flex-shrink-0 flex-grow-0 flex-col flex-wrap items-center gap-2 py-4 text-sm">
       <div
-        className=" grid gap-10 w-body-sm min-w-body  max-w-body flex-wrap items-center
+        className=" relative grid gap-10 w-body-sm min-w-body  max-w-body items-center
                 sm:w-body "
       >
         <h2 className="w-fit mx-auto mt-24 text-center text-lg md:text-2xl font-bold decoration text-txt-mid dark:text-txt-mid-dk">
           London Data Week is made possible by:
         </h2>
-        <div className=" mx-auto w-3/5 max-w-[27rem]">
-          <GroupLogoWhite />
-          <img
-            className="bg-darkblue dark:bg-transparent rounded-2xl "
-            role="presentation"
-            src="./CorporateLogos.png"
-            alt="corporate-logos"
-          />
+        <div className=" relative flex flex-row mx-auto h-[20vw] items-center max-h-[5rem] w-fit max-w-[27rem] gap-6">
+          <a
+            className="w-fit h-full max-h-[5rem] basis-0 grow"
+            href="https://loti.london/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <LotiSvg />
+          </a>
+
+          <HorizontalDivider />
+          <a
+            className="w-fit  h-full max-h-[5rem] basis-0 grow"
+            href="https://www.turing.ac.uk/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AtiSvg />
+          </a>
         </div>
+        <img
+          className="bg-darkblue dark:bg-transparent rounded-2xl w-full  max-w-[27rem] mx-auto"
+          role="presentation"
+          src="./CorporateLogos.png"
+          alt="corporate-logos"
+        />
         <div className="grid gap-4 mb-6">
           <p key="text" className="m-0 w-fit mx-auto text-txt-mid dark:text-txt-mid-dk ">
             Design: London Data Week Team
