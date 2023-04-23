c7998f05ec317413e48ad8ba87242046623534a8
GLD5000 <garethlouisdevlin@gmail.com>
2023-04-23 18:17:14 +0100

    Updated event data parsing

--- src/eventData.ts
+++ src/eventData.ts
@@ -1,3 +1,21 @@
+function lookupMonthName(number: number) {
+  const lookupArray = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  return lookupArray[number - 1];
+}
+
 function objectifyTsv(tableData: string) {
   const [headerString, ...objectValueStrings] = tableData.split(/[\r\n]+/);
   const objectKeys = headerString.replaceAll(/[ ]/g, '').split('\t');
@@ -9,7 +27,10 @@ function objectifyTsv(tableData: string) {
       newObject[`${objectKeys[index]}`] = value.trim();
     });
     const newDay = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(new Date(newObject.Date));
-    newObject.Day = newDay;
+    newObject.dayName = newDay;
+    [newObject.monthNumber, newObject.dayNumber, newObject.yearNumber] = newObject.Date.split('/');
+    const newMonthName = lookupMonthName(Number(newObject.monthNumber));
+    newObject.monthName = newMonthName;
     objectArray.push(newObject);
   });
   sortByDate(objectArray);
@@ -31,8 +52,6 @@ function sortByDate(arrayOfObjects: Array<Record<string, string>>) {
 export default function eventData() {
   const dataTsv = `Name	Organiser	Date	Time	EventType	MoreInfo	Location
   London Data Today & Tomorrow	GLA	7/3/2023	10:00 - 16:00	Public Conversations	https://www.eventbrite.com/e/london-data-week-2023-tickets-618137193987	City Hall
-  bondon Data Today & Tomorrow	GLA	7/3/2023	10:00 - 16:00	Public Conversations	https://www.eventbrite.com/e/london-data-week-2023-tickets-618137193987	City Hall
-  vondon Data Today & Tomorrow	GLA	7/3/2023	10:00 - 16:00	Public Conversations	https://www.eventbrite.com/e/london-data-week-2023-tickets-618137193987	City Hall
   Statisticians for Society: Using stats to supercharge charities	RSS	7/4/2023	10:00 - 16:00	Data Education	https://rss.org.uk/membership/volunteering-and-promoting/statisticians-for-society-initiative/	RSS offices, Errol Street, EC1Y 8LX
   GeoMob	GeoMob	7/5/2023	18:00	Public Conversations	https://thegeomob.com/post/july-5th-2023-geomoblon-details	Geovation Hub
   Cabaret of Dangerous Ideas	Turing,CoDI	7/6/2023	20:00 - 21:30	Exhibits & Experiences	https://www.eventsforce.net/turingevents/263/home	TBD
@@ -42,3 +61,6 @@ export default function eventData() {
   const dataObject = objectifyTsv(dataTsv);
   return dataObject;
 }
+
+// bondon Data Today & Tomorrow	GLA	7/3/2023	10:00 - 16:00	Public Conversations	https://www.eventbrite.com/e/london-data-week-2023-tickets-618137193987	City Hall
+// vondon Data Today & Tomorrow	GLA	7/3/2023	10:00 - 16:00	Public Conversations	https://www.eventbrite.com/e/london-data-week-2023-tickets-618137193987	City Hall
