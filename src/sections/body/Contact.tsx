db1e3474d1ea7b5d8a7e2fe9c660b73b9e9708f0
GLD5000 <garethlouisdevlin@gmail.com>
2023-05-25 20:13:35 +0100

    Updated contact section

--- src/sections/body/Contact.tsx
+++ src/sections/body/Contact.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import useIntersectionObserver from '../../utilities/hooks/useIntersectionObserver';
 import { useIntersectionProviderContext } from '../../utilities/contexts/IntersectionProvider';
 import WritingSvg from '../../assets/icons/WritingSvg';
+import PaperPlaneSvg from '../../assets/icons/PaperPlaneSvg';
 
 export default function Contact() {
   const { elementRef, onScreen } = useIntersectionObserver();
@@ -24,12 +25,14 @@ export default function Contact() {
       ref={elementRef}
       className="flex flex-col gap-12 min-h-screen pt-40 sm:pt-36 snap-none lg:px-20 w-[calc(100vw-1rem)] xs:w-body-sm min-w-body max-w-body sm:w-body mx-auto"
     >
-      <h2 className="text-3xl sm:text-4xl font-bold w-fit mx-auto text-center">@LondonDataWeek Updates</h2>
-      <div className="h-fit min-w-[300px] mx-auto w-fit xs:w-full border-darkpink dark:border-pink shadow-md border-4 rounded-2xl max-w-[50rem] sm:mx-auto">
+      <div className="h-fit min-w-[300px] mx-auto w-fit xs:w-full bg-darkgreen dark:bg-lightgreen border-darkgreen dark:border-lightgreen shadow-md border-4 rounded-2xl max-w-body sm:mx-auto">
+        <h2 className="text-3xl sm:text-4xl font-bold w-full   text-darkblue py-6  mx-auto text-center">
+          @LondonDataWeek Updates
+        </h2>
         <a
           className="twitter-timeline"
           data-width="100%"
-          data-height="600"
+          data-height="700"
           data-theme="dark"
           href="https://twitter.com/londondataweek?ref_src=twsrc%5Etfw"
           target="_blank"
@@ -39,17 +42,18 @@ export default function Contact() {
         </a>
       </div>
 
-      <h2 className="text-3xl sm:text-4xl font-bold w-fit mx-auto">Get in Touch</h2>
-
       <form
-        className="flex flex-col max-w-[50rem] sm:min-w-[18rem] mx-auto w-full  border-4 dark:border-pink border-darkpink shadow-md rounded-xl text-current py-4 px-2 sm:p-4 gap-4"
+        className="flex flex-col max-w-body sm:min-w-[18rem] mx-auto w-full  border-4 dark:border-pink border-darkpink shadow-md rounded-xl text-current gap-4"
         action="https://formsubmit.co/londondataweek@gmail.com"
         method="POST"
       >
-        <div className="h-24 w-32 mx-auto text-darkpink dark:text-pink">
-          <WritingSvg />
+        <div className="grid bg-darkpink dark:bg-pink text-darkblue pb-2">
+          <div className="h-24 w-32 mx-auto ">
+            <WritingSvg />
+          </div>
+          {/* <h3 className="text-2xl mb-2 font-bold w-fit mx-auto">Write a Message</h3> */}
+          <h2 className="text-3xl sm:text-4xl font-bold w-fit mx-auto">Get in Touch</h2>
         </div>
-        <h3 className="text-2xl mb-2 font-bold w-fit mx-auto">Send a Message</h3>
 
         <input
           id="name-input"
@@ -81,9 +85,10 @@ export default function Contact() {
           required
         />
         <button
-          className="mt-8 mb-2 rounded-full w-44 xs:w-60 max-w-[20rem] h-12 text-lg sm:text-xl mx-auto px-4 py-2 font-bold text-darkblue dark:text-darkblue bg-darkpink dark:bg-pink hover:transition focus:transition hover:text-white hover:bg-darkblue  focus:text-white focus:bg-darkblue  dark:hover:bg-white  dark:focus:bg-white"
+          className="my-6 rounded-full w-60 flex flex-row items-center justify-center gap-2 h-12 text-lg sm:text-xl mx-auto px-4 py-2 font-bold text-darkblue dark:text-darkblue bg-darkpink dark:bg-pink hover:transition focus:transition hover:text-white hover:bg-darkblue  focus:text-white focus:bg-darkblue  dark:hover:bg-white  dark:focus:bg-white"
           type="submit"
         >
+          <PaperPlaneSvg />
           Send Message
         </button>
       </form>
